4839bead0bcd3c7d9406069e0056114f76f0512e
Bjørn Atle Vorland <bjorn.atle.vorland@evry.com>
2020-02-29 12:58:06 +0100

    Add project files.

---
+++ se-iac-demo01-webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace se_iac_demo01_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
